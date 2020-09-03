fb07bfc8b2ce762898e4ac258642db9e88e607c5
EdwinMLara <emlara35@gmail.com>
2020-09-03 14:54:20 -0500

    agregando metodos del crud

--- Modelos/Repository.php
+++ Modelos/Repository.php
@@ -2,5 +2,7 @@
 interface Repository{
     public function getAll();
     public function getByForeingKey($arrayToFilter,$functionTofilter);
+    public function deleteById($id);
+    public function insert($arrayArgumentsArray);
 }
 ?>
